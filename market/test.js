f40dcd2c136b2f6df726fe53c8caa25faaf88cd6
kimhyeok <kimmang940715@naver.com>
2024-06-07 21:29:09 +0900

    axios, postman 사용하여 서버통신 구현

--- market/test.js
+++ market/test.js
@@ -1,52 +1,41 @@
-var products = [
-  {
-    name: "농구공",
-    price: 10000,
-    seller: "조던",
-    imageUrl: "images/products/basketball1.jpeg"
-  },
-  {
-    name: "축구공",
-    price: 60000,
-    seller: "메시",
-    imageUrl: "images/products/soccerball1.jpg"
-  },
-  {
-    name: "키보드",
-    price: 10000,
-    seller: "해킹천재",
-    imageUrl: "images/products/keyboard1.jpg"
-  }
-];
+axios
+  .get("https://10dbb377-5ad6-4b2b-98d2-c61953242ce0.mock.pstmn.io/products")
+  .then(function (result) {
+    console.log("통신 결과 : ", result);
+    const products = result.data.products;
 
-let productsHtml = "";
-for (var i = 0; i < products.length; i++) {
-  let product = products[i];
-  productsHtml +=
-    '<div class="products__card">' +
-    "<div>" +
-    '<img class="products__card__01" src="' +
-    product.imageUrl +
-    '"/>' +
-    "</div>" +
-    '<div class="products__card__content">' +
-    '<span class="products__card__content__name">' +
-    product.name +
-    "</span>" +
-    '<span class="products__card__content__price">' +
-    product.price +
-    "원" +
-    " </span>" +
-    '<div class="products__card__content__seller">' +
-    "<span>" +
-    '<img class="products__card__content__sellerimg" src="images/icons/avatar.png"/>' +
-    "</span>" +
-    "<span>" +
-    product.seller +
-    "</span>" +
-    " </div>" +
-    "</div>" +
-    "</div>";
-}
+    let productsHtml = "";
+    for (var i = 0; i < products.length; i++) {
+      let product = products[i];
+      productsHtml +=
+        '<div class="products__card">' +
+        "<div>" +
+        '<img class="products__card__01" src="' +
+        product.imageUrl +
+        '"/>' +
+        "</div>" +
+        '<div class="products__card__content">' +
+        '<span class="products__card__content__name">' +
+        product.name +
+        "</span>" +
+        '<span class="products__card__content__price">' +
+        product.price +
+        "원" +
+        " </span>" +
+        '<div class="products__card__content__seller">' +
+        "<span>" +
+        '<img class="products__card__content__sellerimg" src="images/icons/avatar.png"/>' +
+        "</span>" +
+        "<span>" +
+        product.seller +
+        "</span>" +
+        " </div>" +
+        "</div>" +
+        "</div>";
+    }
 
-document.querySelector(".products").innerHTML = productsHtml;
+    document.querySelector(".products").innerHTML = productsHtml;
+  })
+  .catch(function (error) {
+    console.error("error 발생 : ", error);
+  });
