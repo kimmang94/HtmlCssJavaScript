4c64d6223971532f83dfca563642d5ea1dd7afcf
kimhyeok <kimmang940715@naver.com>
2024-06-10 02:35:04 +0900

    리액트로 변경2

--- react-market/src/main/index.js
+++ react-market/src/main/index.js
@@ -1,33 +1,60 @@
+import React from "react";
 import "./index.css";
+import axios from "axios";
+
 function MainPage() {
+  const [products, setProducts] = React.useState([]);
+  React.useEffect(function () {
+    axios
+      .get(
+        "https://10dbb377-5ad6-4b2b-98d2-c61953242ce0.mock.pstmn.io/products"
+      )
+      .then(function (result) {
+        const products = result.data.products;
+        setProducts(products);
+      })
+      .catch(function (error) {
+        console.error("error : ", error);
+      });
+  }, []);
   return (
     <div>
-      <div class="header">
-        <div class="header__area">{<img src="images/icons/logo.png" />}</div>
+      <div className="header">
+        <div className="header__area">
+          {<img src="images/icons/logo.png" />}
+        </div>
       </div>
-      <div class="body__contents">
-        <div class="body__contents__banner">
+      <div className="body__contents">
+        <div className="body__contents__banner">
           {<img src="images/banners/banner1.png" />}
         </div>
         <h1>판매되는 상품들</h1>
-        <div class="products">
-          <div className="products__card">
-            <div className="products__card__content">
-              <img src="images/products/keyboard1.jpg" />
-              <span className="products__card__content__name">키보드</span>
-              <span className="products__card__content__price">50000 원</span>
-              <div className="products__card__content__seller">
-                <img
-                  className="products__card__content__sellerimg"
-                  src="images/icons/avatar.png"
-                />
-                <span>그랩</span>
+        <div className="products">
+          {products.map(function (product, index) {
+            return (
+              <div className="products__card">
+                <div className="products__card__content">
+                  <img src={product.imageUrl} />
+                  <span className="products__card__content__name">
+                    {product.name}
+                  </span>
+                  <span className="products__card__content__price">
+                    {product.price} 원
+                  </span>
+                  <div className="products__card__content__seller">
+                    <img
+                      className="products__card__content__sellerimg"
+                      src="images/icons/avatar.png"
+                    />
+                    <span>{product.seller}</span>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
-      <div class="footer"></div>
+      <div className="footer"></div>
     </div>
   );
 }
